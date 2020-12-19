d12365d3eae9c1a7cd53369125f654a2e4a75a2c
Kyle Sherrington <kylejsherrington@gmail.com>
2020-12-19 14:01:47 -0800

    Changing some CSS and JS

--- main.js
+++ main.js
@@ -5,13 +5,13 @@ let inputField = document.getElementById("inputField");
 btn.addEventListener("click", function(){
   var paragraph = document.createElement('p');   // creates new paragraph
   paragraph.classList.add('paragraph-styling');
-  paragraph.innerText = inputField.value;
+  paragraph.innerText = inputField.value;   // pass input field value into paragraph
   listContainer.appendChild(paragraph);
-  inputField.value = "";
+  inputField.value = "";   // removes value from input field upon submission
   paragraph.addEventListener("click", function(){
-    paragraph.style.textDecoration = "line-through";
+    paragraph.style.textDecoration = "line-through";   // strikesthrough item with one click
   })
   paragraph.addEventListener("dblclick", function(){
-    listContainer.removeChild(paragraph);
+    listContainer.removeChild(paragraph);     // removes item with double click
   })
 })
